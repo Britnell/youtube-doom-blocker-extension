e406e98a3ba7072ef58217ae7ecc424107593d8a
Britnell <britnell.h-d@partner.akdb.de>
2025-07-18 09:58:41 +0200

    updates

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
-
 # Youtube Doom Blocker
 
 my lil browser extension to block youtube feed of video suggestions on home page (only)
 
-# May we break the cycle
+## May we break the cycle
+
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium enim a eos itaque nobis? Aut ratione esse earum ut eum iusto ab doloremque rerum repellat! Pariatur commodi debitis quis animi!
+
+### Resources
+
+Publishing :
+https://developer.chrome.com/docs/webstore/prepare
+
+Console :
+https://chrome.google.com/u/3/webstore/devconsole/0058ef16-be0d-42ce-bccc-ccb5611fb5eb
--- content.js
+++ content.js
@@ -1,33 +1,36 @@
-
-const isBlockedpage = ()=> window.location.pathname === '/';
-
-const createMessageElement = () => {
-    if (messageElement) return;
-    const msg = document.createElement('div');
-    msg.innerHTML = `
-<div style="height:75vh; display:grid; place-items:center;">
-<h1 style="color:white; font-size: 5vw; text-align:center;">This is your Detox (light)</h1>
-</div>
-    `;
-    msg.style.display = 'none';
-    msg.style.width = '100%';
-    msg.id = 'yt-doom-block-msg'; 
-    messageElement = msg;
-    const parent = document.querySelector('ytd-page-manager')
-    parent.appendChild(messageElement);
-};
-
 let messageElement;
 
 document.addEventListener('yt-navigate-finish', () => {
-    createMessageElement();
-    const grid = document.querySelector('ytd-browse')
-    grid.style.display = isBlockedpage() ? 'none' : 'block';
-    messageElement.style.display = isBlockedpage() ? 'block' : 'none';
+  createMessageElement();
+  const grid = document.querySelector('ytd-browse');
+  if (!grid) return;
+  const block = isBlockedpage();
+  grid.style.display = block ? 'none' : 'block';
+  messageElement.style.display = block ? 'block' : 'none';
 });
 
-
 document.addEventListener('yt-navigate-start', () => {
-    if(!isBlockedpage())
-        messageElement.style.display = 'none';
-})
+  if (!isBlockedpage()) {
+    messageElement.style.display = 'none';
+  }
+});
+
+function isBlockedpage() {
+  window.location.pathname === '/';
+}
+
+function createMessageElement() {
+  if (messageElement) return;
+  const msg = document.createElement('div');
+  msg.innerHTML = `
+  <div style="height:75vh; display:grid; place-items:center;">
+  <h1 style="color:white; font-size: 5vw; text-align:center;">This is your Detox (light)</h1>
+  </div>
+      `;
+  msg.style.display = 'none';
+  msg.style.width = '100%';
+  msg.id = 'yt-doom-block-msg';
+  messageElement = msg;
+  const parent = document.querySelector('ytd-page-manager');
+  parent?.appendChild(messageElement);
+}
