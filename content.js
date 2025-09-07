a137963e9a381d9adff2436592815ed377b218df
Britnell <britnell.h-d@partner.akdb.de>
2025-09-07 12:03:11 +0200

    fix buggies

--- content.js
+++ content.js
@@ -1,36 +1,50 @@
 let messageElement;
 
+window.addEventListener('load', () => {
+  feedblocker();
+});
+
 document.addEventListener('yt-navigate-finish', () => {
-  createMessageElement();
-  const grid = document.querySelector('ytd-browse');
-  if (!grid) return;
-  const block = isBlockedpage();
-  grid.style.display = block ? 'none' : 'block';
-  messageElement.style.display = block ? 'block' : 'none';
+  feedblocker();
 });
 
 document.addEventListener('yt-navigate-start', () => {
-  if (!isBlockedpage()) {
-    messageElement.style.display = 'none';
-  }
+  feedblocker();
 });
 
+function feedblocker() {
+  createMessageElement();
+
+  const block = isBlockedpage();
+  const grid = document.querySelector('ytd-browse');
+  if (grid) {
+    grid.style.display = block ? 'none' : 'block';
+  }
+  const preview = document.querySelector('ytd-video-preview');
+  if (preview) {
+    preview.style.display = block ? 'none' : 'block';
+  }
+
+  messageElement.style.display = block ? 'block' : 'none';
+}
+
 function isBlockedpage() {
-  window.location.pathname === '/';
+  return window.location.pathname === '/';
 }
 
 function createMessageElement() {
   if (messageElement) return;
+
   const msg = document.createElement('div');
-  msg.innerHTML = `
-  <div style="height:75vh; display:grid; place-items:center;">
-  <h1 style="color:white; font-size: 5vw; text-align:center;">This is your Detox (light)</h1>
-  </div>
-      `;
+  msg.innerHTML = `<div style="height:75vh; display:grid; place-items:center;">
+  <h1 style="color:white; font-size: 5vw; text-align:center;">This is your detox (light)</h1>
+  </div>`;
   msg.style.display = 'none';
   msg.style.width = '100%';
-  msg.id = 'yt-doom-block-msg';
-  messageElement = msg;
+
   const parent = document.querySelector('ytd-page-manager');
-  parent?.appendChild(messageElement);
+  if (parent) {
+    parent?.appendChild(messageElement);
+    messageElement = msg;
+  }
 }
