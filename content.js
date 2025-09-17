5c0c07efd80bb00aa6524a37a35eaf97753fc601
Britnell <britnell.h-d@partner.akdb.de>
2025-09-17 10:03:07 +0200

    cleans

--- content.js
+++ content.js
@@ -1,20 +1,38 @@
+window.addEventListener('load', feedblocker);
+// document.addEventListener('yt-navigate-start', feedblocker);
+document.addEventListener('yt-navigate-finish', feedblocker);
+
 let messageElement;
-
-window.addEventListener('load', () => {
-  feedblocker();
-});
-
-document.addEventListener('yt-navigate-finish', () => {
-  feedblocker();
-});
-
-document.addEventListener('yt-navigate-start', () => {
-  feedblocker();
-});
 
 function feedblocker() {
   createMessageElement();
 
+  hideHomeFeed();
+
+  hideSidebarSuggestions();
+
+  hideShorts();
+}
+
+function hideSidebarSuggestions() {
+  const isVideoPage = window.location.pathname.startsWith('/watch');
+
+  const el = document.querySelector('ytd-watch-next-secondary-results-renderer');
+  if (el) {
+    el.style.display = isVideoPage ? 'none' : 'block';
+  }
+}
+
+function hideShorts() {
+  const isShort = window.location.pathname.startsWith('/shorts');
+  const el = document.querySelector('ytd-shorts');
+  if (el) {
+    el.innerHTML = '';
+    // el.style.display = isShort ? 'none' : 'block';
+  }
+}
+
+function hideHomeFeed() {
   const block = isBlockedpage();
   const grid = document.querySelector('ytd-browse');
   if (grid) {
@@ -24,7 +42,6 @@ function feedblocker() {
   if (preview) {
     preview.style.display = block ? 'none' : 'block';
   }
-
   messageElement.style.display = block ? 'block' : 'none';
 }
 
